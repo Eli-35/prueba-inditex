9c6043bb76a69361908bf06352de63c514c8253d
Eli-35 <elias1990@msn.com>
2024-10-18 16:04:58 +0200

    Fix: improved service unit tests. Updated readme.

--- src/test/java/com/inditex/prices/domain/service/DomainPriceServiceUnitTest.java
+++ src/test/java/com/inditex/prices/domain/service/DomainPriceServiceUnitTest.java
@@ -22,15 +22,48 @@ public class DomainPriceServiceUnitTest {
     private PriceRepository repository;
     private DomainPriceService service;
 
+    private final Price createTwoPricesWithDifferentPriority(LocalDateTime date, int brandId, int productId) {
+        final int priority = 9;
+        final double priceValue = 28.99;
+        // crear dos precios de ejemplo
+        final Price priceLessPriority = Price.builder()
+                .brandId(brandId)
+                .startDate(date.minusDays(2))
+                .endDate(date.plusDays(2))
+                .productId(productId)
+                .priority(priority)
+                .price(priceValue)
+                .currency("EUR")
+                .build();
+        final Price priceMorePriority = Price.builder()
+                .brandId(brandId)
+                .startDate(date.minusDays(1))
+                .endDate(date.plusDays(3))
+                .productId(productId)
+                .priority(priority + 1)
+                .price(priceValue - 2)
+                .currency("EUR")
+                .build();
+
+        service.createPrice(priceLessPriority);
+        service.createPrice(priceMorePriority);
+
+        // verificación redundante
+        verify(repository, times(2)).save(any(Price.class));
+
+        // devolver el de mayor prioridad
+        return priceMorePriority;
+    }
+
     @BeforeEach
     void setUp() {
         repository = mock(PriceRepository.class);
         service = new DomainPriceService(repository);
     }
 
-    // @Disabled
     @Test
-    void shouldCreatePrice_thenSave() {
+    void givenNewPrice_whenCreatePrice_thenSave() {
+        // given
         final Price price = Price.builder()
                 .brandId(2)
                 .startDate(LocalDateTime.now().minusDays(2))
@@ -41,8 +74,10 @@ public class DomainPriceServiceUnitTest {
                 .currency("EUR")
                 .build();
 
+        // when
         service.createPrice(price);
 
+        // then
         verify(repository).save(any(Price.class));
     }
 
@@ -52,38 +87,16 @@ public class DomainPriceServiceUnitTest {
         final LocalDateTime date = LocalDateTime.now();
         final int brandId = 2;
         final int productId = 27;
-        final double priceValue = 28.99;
-        final Price price1 = Price.builder()
-                .brandId(brandId)
-                .startDate(date.minusDays(2))
-                .endDate(date.plusDays(2))
-                .productId(productId)
-                .priority(9)
-                .price(priceValue)
-                .currency("EUR")
-                .build();
-        service.createPrice(price1);
-
-        final Price price2 = Price.builder()
-                .brandId(brandId)
-                .startDate(date.minusDays(1))
-                .endDate(date.plusDays(3))
-                .productId(productId)
-                .priority(10)
-                .price(priceValue - 2)
-                .currency("EUR")
-                .build();
-        service.createPrice(price2);
-        verify(repository, times(2)).save(any(Price.class));
+        final Price prioritaryPrice = createTwoPricesWithDifferentPriority(date, brandId, productId);
 
         // when
-        when(repository.findApplicablePrice(date, productId, brandId)).thenReturn(Optional.of(price2));
+        when(repository.findApplicablePrice(date, productId, brandId)).thenReturn(Optional.of(prioritaryPrice));
         Price applicablePrice = service.getApplicablePrice(date, productId, brandId);
 
         // then
         assertNotNull(applicablePrice);
-        assertEquals(price2.getPrice(), applicablePrice.getPrice());
-        assertEquals(price2.getPriceId(), applicablePrice.getPriceId());
+        // validar que el precio coincide con el de mayor prioridad
+        assertEquals(prioritaryPrice.getPrice(), applicablePrice.getPrice());
+        assertEquals(prioritaryPrice.getPriceId(), applicablePrice.getPriceId());
     }
-
 }
